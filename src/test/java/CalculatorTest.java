3d9a812230ae9d3607a6a695d0f3d7abb58fc3e0
Вадим QA <vadim.z.qa@innofin.team>
2026-03-18 20:08:49 +0300

    First test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testSum() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
